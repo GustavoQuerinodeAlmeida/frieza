50d20bcde192152163853ff27a3f2fa4d5353901
O-Guiih <guiihsilvasousa1@gmail.com>
2026-03-06 16:30:33 -0300

    modo escuro

--- js/inicio.js
+++ js/inicio.js
@@ -1,15 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+
 const container = document.querySelector(".noticias");
 
-document.getElementById("nextBtn").addEventListener("click", () => {
+// botão de rolar notícias
+const nextBtn = document.getElementById("nextBtn");
+
+if(nextBtn){
+nextBtn.addEventListener("click", () => {
     container.scrollBy({
         left: 300,
         behavior: "smooth"
     });
 });
+}
 
-document.getElementById("prevBtn").addEventListener("click", () => {
-    container.scrollBy({
-        left: -300,
-        behavior: "smooth"
-    });
+// botão modo escuro
+const botao = document.getElementById("modo");
+
+if(botao){
+botao.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
+}
+
 });
