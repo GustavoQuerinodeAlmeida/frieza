0db6b3fca267e711a6f9bd5a08c3100586f448b6
GustavoQuerinodeAlmeida <gustavo.querino2502@gmail.com>
2026-03-17 16:06:26 -0300

    mudanças

--- js/geral.js
+++ js/geral.js
@@ -1,10 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const botao = document.getElementById("modo");
-    
+
     // pega tema salvo
     let temaSalvo = localStorage.getItem("theme");
-    
+
+    // função pra trocar ícone
+    function atualizarIcone() {
+        if (document.body.classList.contains("dark-mode")) {
+            botao.innerHTML = '<i data-lucide="sun"></i>';
+        } else {
+            botao.innerHTML = '<i data-lucide="moon"></i>';
+        }
+
+        lucide.createIcons(); // renderiza o ícone
+    }
+
     // se existir tema salvo
     if(temaSalvo === "dark"){
         document.body.classList.add("dark-mode");
@@ -13,31 +24,31 @@ document.addEventListener("DOMContentLoaded", () => {
         document.body.classList.remove("dark-mode");
     }
     else{
-    
         // primeira visita → usa modo do navegador
         if(window.matchMedia("(prefers-color-scheme: dark)").matches){
             document.body.classList.add("dark-mode");
         }
-    
     }
-    
+
+    // atualiza ícone ao carregar
+    atualizarIcone();
+
     // botão
     if(botao){
-    
+
         botao.addEventListener("click", () => {
-    
+
             document.body.classList.toggle("dark-mode");
-    
+
             if(document.body.classList.contains("dark-mode")){
                 localStorage.setItem("theme","dark");
-                botao.textContent = "☀️";
             }else{
                 localStorage.setItem("theme","light");
-                botao.textContent = "🌙";
             }
-    
+
+            atualizarIcone(); // troca ícone
         });
-    
+
     }
-    
-    });
+
+});
